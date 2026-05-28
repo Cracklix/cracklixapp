5b14f164f43e1e3d2cabcd776a0fc31fa37dc290
Cracklix <cracklixhelp@gmail.com>
2026-05-28 04:31:27 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCXXoUGojTRz0iuxzispTCrasNPRJDXjJA",
+  authDomain: "studio-4007402736-dd858.firebaseapp.com",
+  projectId: "studio-4007402736-dd858",
+  storageBucket: "studio-4007402736-dd858.firebasestorage.app",
+  messagingSenderId: "130551790638",
+  appId: "1:130551790638:web:c95ca3ac844dc031bdb3bf",
+  measurementId: "G-XXXXXXXXXX"
 };
